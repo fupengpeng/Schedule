aa7d9f5afe7f3e739df5a452ceafec09182e2a69
564055710@qq.com <564055710@qq.com>
2018-03-22 16:08:03 +0800

    Initial commit

---
+++ app/src/main/java/com/zhisimina/schedule/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.zhisimina.schedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @description  主界面
+ * @author  fupengpeng
+ * @date  2018/3/21 0021 17:14
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
